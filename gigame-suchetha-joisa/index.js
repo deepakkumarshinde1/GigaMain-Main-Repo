468874524fcf3e2baa017a789b5df635b0b495d6
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2023-05-25 11:22:48 +0530

    Async In Javascript

--- gigame-suchetha-joisa/index.js
+++ gigame-suchetha-joisa/index.js
@@ -1,35 +1,110 @@
-// promise
-// deepak is present
-//    thus there will session
+// promises
+// handel exception
+// async in nature ==> non blocking
 
-// older way
-// promise ==> true ===> resolve ==> .then(()=>{})
-// promise ==> false ===> reject ==> .catch(()=>{})
+// old promise ==> Promise
+function div(numberOne, numberTwo) {
+  return new Promise((resolve, reject) => {
+    if (numberTwo === 0) {
+      // error :: Cannot divide by zero
+      // rejection of promise
+      reject("Cannot divide by zero");
+    } else {
+      let result = numberOne / numberTwo;
+      // correct
+      // resolve of promise
+      resolve(result);
+    }
+  });
+}
+div(10, 0)
+  .then((data) => {
+    console.log("result ", data);
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
+// new promise ==> async
 
-// new way
-// promise ==> true ===> resolve ==> try{ ... }
-// promise ==> false ===> reject ==> catch(error){}
+async function divTwo(numberOne, numberTwo) {
+  if (numberTwo === 0) {
+    // error :: Cannot divide by zero
+    // rejection of promise
+    return Promise.reject("Cannot divide by zero");
+  } else {
+    let result = numberOne / numberTwo;
+    // correct
+    // resolve of promise
+    return Promise.resolve(result);
+  }
+}
 
-// Before es6 ==> new Promise ==> async in nature
+// run a single
+divTwo(10, 0)
+  .then((data) => {
+    console.log("result ", data);
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
 
-// try {
-// } catch (error) {} // sync in nature
+// run multi Promise in older way
+divTwo(10, 10)
+  .then((data) => {
+    console.log("result-1 ", data);
+    return div(10, 0);
+  })
+  .then((data) => {
+    console.log("result-2 ", data);
+  })
+  .catch((error) => {
+    console.error("error ", error);
+  });
 
-// JS run only on one thread
-// sync ==> code is block
+// run multiple promise ==> async await
+// await ==> run promises one by one
 
-// event loop (async)
-// web api ==> async
-//    promise , async/await , DOM (document object model),
-//    fetch (ajax) , xhr (ajax) , setTimeout , setInterval
-console.log("file is uploading on server");
-setTimeout(() => {
-  console.log("file uploaded in sec 10s");
-}, 10 * 1000); // 1s === 1000 ms
+async function calc() {
+  try {
+    let resultOne = await divTwo(10, 10);
+    let resultTwo = await div(10, 20);
+    console.log("result-1 ", resultOne);
+    console.log("result-2 ", resultTwo);
+  } catch (error) {
+    console.error("error ", error);
+  }
+}
 
-console.log("make a payment started on server");
-setTimeout(() => {
-  console.log("payment done in sec 30s");
-}, 30 * 1000); // 1s === 1000 ms
+calc();
 
-console.log("watch a video");
+// IIFE ==> Immediate Invoke function express
+(async () => {
+  try {
+    let resultOne = await divTwo(10, 10);
+    let resultTwo = await div(10, 20);
+    console.log("result-1 ", resultOne);
+    console.log("result-2 ", resultTwo);
+  } catch (error) {
+    console.error("error ", error);
+  }
+})();
+
+// callback hell
+// handel error
+// complex syntax
+div(10, 0, () => {
+  mul(10, 50, () => {
+    sub(10, 50, () => {
+      printDiv(10, 50, () => {
+        div(10, 50, () => {
+          div(10, 50, () => {});
+        });
+      });
+    });
+  });
+});
+
+// local storage
+// basic node js
+// npm and commands
+// react
